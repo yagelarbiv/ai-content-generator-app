f500d092dd8fa8a6506b202a014cf42510f2f531
yagelarbiv <142971084+yagelarbiv@users.noreply.github.com>
2024-07-21 17:36:12 +0300

    Adding rich text editor for output

--- app/dashboard/content/_components/OutputSection.tsx
+++ app/dashboard/content/_components/OutputSection.tsx
@@ -1,10 +1,26 @@
 "use client"
-
+import '@toast-ui/editor/dist/toastui-editor.css';
+import { Editor } from '@toast-ui/react-editor';
+import { Copy } from 'lucide-react';
+import { Button } from "@/components/ui/button";
+import { useRef } from 'react';
 
 function OutputSection() {
+  const editorRef:any = useRef();
   return (
-    <div>
-      Output-Section
+      <div className='bg-white shadow-lg border rounded-lg'>
+        <div className='flex justify-between item-center p-5'>
+          <h2 className='font-medium text-lg'>Your Result</h2>
+          <Button className='flex gap-2'> <Copy className='w-4 h-4'/> copy</Button>
+        </div>
+      <Editor
+        ref={editorRef}
+        initialValue="Your Result will apear here"
+        initialEditType="wysiwyg"
+        height="600px"
+        useCommandShortcut={true}
+        onChange={() => console.log(editorRef.current.getInstance().getMarkdown())}
+      />
     </div>
   );
 };
